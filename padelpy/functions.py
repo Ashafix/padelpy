917b4e5a96141a7260216540489d293818c7a1a1
Ashafix <maximili.peters@mail.huji.ac.il>
2025-05-24 12:31:19 +0200

    removed print statement

--- padelpy/functions.py
+++ padelpy/functions.py
@@ -63,7 +63,6 @@ def from_smiles(smiles,
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")#[:-3]
 
     with tempfile.NamedTemporaryFile(mode='w', suffix='.smi', delete_on_close=False) as smi_file:
-        print(smi_file.name)
         if type(smiles) == str:
             smi_file.write(smiles)
         elif type(smiles) == list:
